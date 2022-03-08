4176574b5c73b51c860f6f3fbefab848e367d1c0
Augustin <educ.lecomte@gmail.com>
2022-03-08 13:16:25 +0100

    modif mineur

--- lib/affichepage.dart
+++ lib/affichepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_api_connect/profil.dart';
 
 class AffichePage extends StatefulWidget {
   const AffichePage({Key? key, required this.title}) : super(key: key);
--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_api_connect/profil.dart';
 
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
